d3e528ef63ca1aeeb494f290473229797955d05d
Hyun Ku Kim <presentnine@naver.com>
2021-08-03 21:33:20 +0900

    initialize

---
+++ src/main/java/com/surilaw/SurilawApplication.java
@@ -0,0 +1,13 @@
+package com.surilaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurilawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurilawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surilaw/SurilawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surilaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurilawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
