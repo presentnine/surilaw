7b61afe6fc0dd16304fa5945de947998bbab77d4
howtolivelikehuman <yoich_@naver.com>
2021-08-11 23:13:45 +0900

    게시글 작성, 수정, 삭제

---
+++ src/main/java/ga/surilaw/controller/board/BoardController.java
@@ -0,0 +1,35 @@
+package ga.surilaw.controller.board;
+
+import ga.surilaw.common.mapper.PostInfoMapper;
+import ga.surilaw.dto.InsertPostInfoDto;
+import ga.surilaw.entity.PostInformation;
+import ga.surilaw.service.board.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final PostInfoMapper postInfoMapper;
+    private final BoardService boardService;
+
+    @PostMapping("/api/board/insert")
+    public void writePost(@RequestBody InsertPostInfoDto insertPostInfoDto){
+        //memberId 넣어줘야함.
+        PostInformation postInformation =postInfoMapper.insertDtoToEntity(insertPostInfoDto);
+        boardService.write(postInformation);
+    }
+
+    @PutMapping("/api/board/update")
+    public void updatePost(@RequestBody InsertPostInfoDto insertPostInfoDto){
+        //memberId 넣어줘야함.
+        PostInformation postInformation =postInfoMapper.insertDtoToEntity(insertPostInfoDto);
+        boardService.update(postInformation);
+    }
+
+    @DeleteMapping("/api/board/delete/{postId}")
+    public void deletePost(@PathVariable(name = "postId") Long postId){
+        boardService.delete(postId);
+    }
+}
