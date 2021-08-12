156a53df19c0795c8aca43c56544945fd0289edb
HowtoLiveLikehuman <63439738+howtolivelikehuman@users.noreply.github.com>
2021-08-12 22:47:46 +0900

    Revert "게시글 작성, 수정, 삭제"

--- src/main/java/ga/surilaw/service/board/BoardService.java
+++
@@ -1,9 +0,0 @@
-package ga.surilaw.service.board;
-
-import ga.surilaw.entity.PostInformation;
-
-public interface BoardService {
-    Long write(PostInformation postInformation);
-    Long update(PostInformation postInformation);
-    void delete(Long postId);
-}
